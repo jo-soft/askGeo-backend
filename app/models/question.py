e714da693585941b994e8b99af5c589980840296
johannes <ichhabekeineemail@gmx.net>
2018-01-20 18:00:14 +0100

    adjusted question model to new base model

--- app/models/question.py
+++ app/models/question.py
@@ -1,5 +1,21 @@
+from marshmallow import fields
+
 from models.model_base import ModelBase
 
 
 class QuestionModel(ModelBase):
-    pass
+    @classmethod
+    def get_scheme(cls, class_to_create=None):
+        class_to_create = class_to_create or cls
+        base_schema = super(QuestionModel, cls).get_scheme(class_to_create)
+
+        class QuestionSchema(base_schema):
+            topic = fields.String()
+            question = fields.String()
+            score = fields.Integer(default=0)
+
+        return QuestionSchema
+
+    def __init__(self,
+                 **kwargs):
+        super().__init__(**kwargs)
